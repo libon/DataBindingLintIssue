9fa4d34c0b2912e1bdb13af074fe9851d34a0145
Carmen Alvarez <calvarez@libon.com>
2019-02-13 17:43:41 +0100

    Use databinding.

--- app/src/main/java/com/example/databindinglintissue/MainActivity.kt
+++ app/src/main/java/com/example/databindinglintissue/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.databindinglintissue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.databindinglintissue.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
